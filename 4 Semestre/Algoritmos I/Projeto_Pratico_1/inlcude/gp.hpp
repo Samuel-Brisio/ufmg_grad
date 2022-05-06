a42ed9079e8749dfeb062ca0e85bd16d39b5fce6
Samuel Brisio <samuelbrisio@outlook.com>
2022-05-06 17:28:44 -0300

    Gale Shapley implementation done

--- 4 Semestre/Algoritmos I/Projeto_Pratico_1/inlcude/gp.hpp
+++ 4 Semestre/Algoritmos I/Projeto_Pratico_1/inlcude/gp.hpp
@@ -1,7 +1,10 @@
 #ifndef GPHPP
 #define GPHPP
 
+#include <cassert>
 #include <iostream>
+#include <map>
+#include <queue>
 #include <vector>
 
 class GaleShapley {
@@ -10,16 +13,27 @@ class GaleShapley {
             std::vector <char> proposeSideList,
             std::vector < int > passiveSideList,
             std::vector < std::vector < int > > proposeSidePreference,
-            std::vector < std::vector < int > > passiveSidePreference
+            std::vector < std::vector < char > > passiveSidePreference
             );
 
         void gpExecution();
         void printResult();
 
     private:
+        std::vector <char> _proposeSideList;
+        std::vector < int > _passiveSideList;
+        std::vector < std::vector < int > > _proposeSidePreference;
+        std::vector < std::vector < char > > _passiveSidePreference;
+
+        std::queue <char> unmatch;
+        std::map < char, int > track;
+        std::map < char, int > proposeSideSolution;
+        std::map < int, char > passiveSideSolution; 
 
         void inicializeUnmatchConteiner();
-
+        void inicializeMaps();
+        int findNextPassiveEntity(char proposeEnt); // find the next passive entity on the propose preference list
+        bool passivePrefersProposer(char proposeEnt, int passiveEnt); // verify if the passive like the new propose more than the old one
 };
 
 #endif
---
+++ 4 Semestre/Algoritmos I/Projeto_Pratico_1/src/gp.cpp
@@ -0,0 +1,82 @@
+#include "gp.hpp"
+
+GaleShapley::GaleShapley(
+    std::vector <char> proposeSideList,
+    std::vector < int > passiveSideList,
+    std::vector < std::vector < int > > proposeSidePreference,
+    std::vector < std::vector < char > > passiveSidePreference
+) {
+
+_proposeSideList = proposeSideList;
+_passiveSideList = passiveSideList;
+_proposeSidePreference = proposeSidePreference;
+_passiveSidePreference = passiveSidePreference;
+
+inicializeUnmatchConteiner();
+}
+
+void GaleShapley::inicializeUnmatchConteiner() {
+    for(auto elem: _proposeSideList) {
+        unmatch.push(elem);
+    }
+}
+
+int GaleShapley::findNextPassiveEntity(char proposeEnt) {
+    int idxPropose = proposeEnt - 'a';
+    int idxNextPassive = track[proposeEnt];
+    
+    return _proposeSidePreference[idxPropose][idxNextPassive]; 
+}
+
+bool GaleShapley::passivePrefersProposer(char proposeEnt, int passiveEnt) {
+    char passiveActualMatch = passiveSideSolution[passiveEnt];
+
+    for(auto e: _passiveSidePreference[passiveEnt]) {
+        if( e == passiveActualMatch) return false;
+        if( e == proposeEnt) return true;
+    }
+
+    assert(false); // if this instruction is executed something is very wrong
+}
+
+void GaleShapley::gpExecution() {
+    while(unmatch.size() != 0) {
+        char proposeEntity = unmatch.front();
+        unmatch.pop();
+        
+        if (track.find(proposeEntity) == track.end()) track.insert(std::pair < char, int > (proposeEntity, 0));
+        
+        int passiveEntity = findNextPassiveEntity(proposeEntity);
+
+        // passive is unmatch
+        if ( passiveSideSolution.find(passiveEntity) == passiveSideSolution.end() ) { 
+            proposeSideSolution.insert_or_assign(proposeEntity, passiveEntity);
+            passiveSideSolution.insert_or_assign(passiveEntity, proposeEntity);
+        }
+
+        else {
+            bool willChange = passivePrefersProposer(proposeEntity, passiveEntity);
+
+            // passive prefers the new proposer
+            if(willChange) {
+                char oldProposeEntity = passiveSideSolution[passiveEntity];
+                track[oldProposeEntity]++;
+                
+                passiveSideSolution[passiveEntity] = proposeEntity;
+                proposeSideSolution.insert_or_assign(proposeEntity, passiveEntity);
+            }
+
+            // passive will not change his patner
+            else {
+                unmatch.push(proposeEntity);
+                track[proposeEntity]++;
+                assert(track[proposeEntity] < _passiveSideList.size()); // check if proposeEntity is unmatch after propose to all the passives
+            }
+        }
+    }
+}
+
+
+void printResult() {
+
+}
